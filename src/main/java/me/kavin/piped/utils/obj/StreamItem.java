9c2f606906ac6cd4552b7fbb291dc3bc0f911609
damianfurrer <damianfurrer@bluewin.ch>
2022-07-31 22:37:05 +0200

    send clean Id along streamItem

--- src/main/java/me/kavin/piped/utils/obj/StreamItem.java
+++ src/main/java/me/kavin/piped/utils/obj/StreamItem.java
@@ -2,12 +2,13 @@ package me.kavin.piped.utils.obj;
 
 public class StreamItem {
 
-    public String url, title, thumbnail, uploaderName, uploaderUrl, uploaderAvatar, uploadedDate, shortDescription;
+    public String id, url, title, thumbnail, uploaderName, uploaderUrl, uploaderAvatar, uploadedDate, shortDescription;
     public long duration, views, uploaded;
     public boolean uploaderVerified;
 
     public StreamItem(String url, String title, String thumbnail, String uploaderName, String uploaderUrl,
                       String uploaderAvatar, String uploadedDate, String shortDescription, long duration, long views, long uploaded, boolean uploaderVerified) {
+        this.id = url.replaceFirst("/watch\\?v=","");
         this.url = url;
         this.title = title;
         this.thumbnail = thumbnail;
